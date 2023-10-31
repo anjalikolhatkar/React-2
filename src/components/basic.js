759b82e9651a2aadc88883f7ccc3d2fe9061dc7f
anjali_kolhatkar <anjalikolhatkar1803@gmail.com>
2023-10-31 22:53:54 +0530

    first-v

---
+++ src/components/basic.js
@@ -0,0 +1,18 @@
+import "./style.css"
+
+function Basic()
+{
+    return(
+        <section>
+            
+        <div class="main">
+            <h1>Calculator Application </h1>
+            <h3>Made with ❤️ by: </h3>
+            <h3>Anjali Kolhatkar - 21BCE11171</h3>
+            
+        </div>
+        </section>
+    )
+}
+
+export default Basic;
